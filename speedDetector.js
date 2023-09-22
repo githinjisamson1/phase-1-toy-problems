f23eb4dd6f4954d23370576760ddef009b316e26
githinjisamson1 <githinjisamson148@gmail.com>
2023-09-22 04:26:31 +0300

    Refactor speedDetector.js to include function

--- speedDetector.js
+++ speedDetector.js
@@ -3,15 +3,20 @@ let demeritPoints = 0;
 
 // convert user input to an integer
 let speedOfCar = parseInt(prompt("enter speed of car"));
-if (speedOfCar < speedLimit) {
-  // within speed limit range
-  console.log("OK");
-} else {
-  let kmAboveLimit = speedOfCar - speedLimit; //kilometers above speed limit
-  let points = kmAboveLimit / 5; //points per 5 kilometer
-  demeritPoints += points; //demeritPoints = demeritPoints + points
-  console.log(`Total number of demerit points: ${Math.floor(demeritPoints)}`); //truncate decimal part
-  if (demeritPoints > 12) {
-    console.log("license suspended");
+
+const displayDemeritPoints = (speedOfCar) => {
+  if (speedOfCar < speedLimit) {
+    // within speed limit range
+    console.log("OK");
+  } else {
+    let kmAboveLimit = speedOfCar - speedLimit; //kilometers above speed limit
+    let points = kmAboveLimit / 5; //points per 5 kilometer
+    demeritPoints += points; //demeritPoints = demeritPoints + points
+    console.log(`Total number of demerit points: ${Math.floor(demeritPoints)}`); //truncate decimal part
+    if (demeritPoints > 12) {
+      console.log("license suspended");
+    }
   }
-}
+};
+
+displayDemeritPoints(speedOfCar);
